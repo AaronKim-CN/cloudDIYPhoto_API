d0eb0f301eebf8b6b418ac6e25e2565b96b9d95f
AaronKim-CN <aronkim.cn@gmail.com>
2021-05-27 17:25:15 +0900

    aaa

--- shScripts/app_start.sh
+++ shScripts/app_start.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 cd /home/ec2-user/nodetest/api
 sudo pm2 delete "cloudDIY_Photo_API"
-sudo pm2 start npm --name "cloudDIY_Photo_API" -- start
+sudo pm2 start npm --name "cloudDIY_Photo_API" -- start --watch
 sudo pm2 startup
 sudo pm2 save
